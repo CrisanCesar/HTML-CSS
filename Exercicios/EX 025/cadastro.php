58f5082c4b593060d20ce7bc033af8ea92c4eedc
GGrare <cesarcrisan705@gmail.com>
2026-05-18 21:11:31 -0300

    Delete cadastro.php

--- Exercicios/EX 025/cadastro.php
+++
@@ -1,2 +0,0 @@
-Os dados foram inviados com sucesso!
-Aprenda php para criar um sistema de cadastro completo, com validação de dados, conexão com banco de dados e muito mais. Acesse nosso curso online e comece a aprender hoje mesmo!
